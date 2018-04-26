7ffc038967b5ac09334f5c9b1f11fecab4bb05e6
John Crisostomo <johncrisostomo@protonmail.ch>
2018-04-26 15:19:10 +0800

    Implements genesis block

--- block.js
+++ block.js
@@ -6,6 +6,15 @@ class Block {
         this.data = data;
     }
 
+    static genesis() {
+        return new this(
+            Date.now(),
+            '1dd994f61ee12cf8140f146d193041fc07d1efe7aebd259ed5bdac4e873d96b3',
+            '2c521b0e01c6c3568e0fa74b623aa1852eabd15bf6425c40884b0092a9067272',
+            []
+        );
+    }
+
     toString() {
         return `Block - 
             Timestamp: ${this.timestamp}
