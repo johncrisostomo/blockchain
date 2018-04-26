d7be2c9d0065f251f60d5e7dfee9fb71519f40ac
John Crisostomo <johncrisostomo@protonmail.ch>
2018-04-26 15:51:58 +0800

    Replace caveman testing with real testing

---
+++ Block.test.js
@@ -0,0 +1,21 @@
+const Block = require('./Block');
+
+describe('Block', () => {
+    let data;
+    let previousData;
+    let block;
+
+    beforeEach(() => {
+        data = 'dunno';
+        previousBlock = Block.genesis();
+        block = Block.mineBlock(previousBlock, data);
+    });
+
+    it('sets `data` to equal input', () => {
+        expect(block.data).toEqual(data);
+    });
+
+    it('sets `previousHash` to equal previous block', () => {
+        expect(block.previousHash).toEqual(previousBlock.hash);
+    });
+});
--- test.js
+++
@@ -1,4 +0,0 @@
-const Block = require('./block');
-
-const b = Block.mineBlock(Block.genesis(), 'b');
-console.log(b.toString());
